3dbdfb3cf0c79a518c8d3c408c5548f57d7eb0ba
Beki <uzbekitube@gmail.com>
2020-11-30 10:07:46 +0900

    added comments

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
-document.addEventListener("DOMContentLoaded", DomLoaded, false)
+/* Reversie Game made in HTML/CSS/JS */
 
+// wait for the DOM to load first
+document.addEventListener("DOMContentLoaded", DomLoaded, false);
+
+// DOMがloadした後実行される関数
 function DomLoaded(e) {
-    const table = document.getElementById("game-table")
-    // const empty_piece = document.querySelectorAll("td.empty") //all empty pices
+    const table = document.getElementById("game-table");　 // テーブル
+    const table_length = document.getElementById("game-table").rows[0].cells.length;　 //テーブルのサイズ
+    const td = document.querySelectorAll("td"); // 全てのtdタッグ
+    let game_over = false;
 
-    const table_length = document.getElementById("game-table").rows[0].cells.length
-    const td = document.querySelectorAll("td")
-    let game_over = false
+    // 横、縦、斜めの確認に必要のある方向の計算し方
     const directions = [
         [0, -1],
         [0, +1],
@@ -16,92 +20,97 @@ function DomLoaded(e) {
         [+1, +1],
         [+1, -1],
         [-1, +1],
-    ]
+    ];
 
+    // プレイヤクラス
     class Player {
         constructor(color) {
-            this.color = color
-            this.all_pieces = document.querySelectorAll(`p.${this.color}`)
-        }
-    }
-    const black = new Player("black")
-    const white = new Player("white")
+            this.color = color; //プレイヤの色
+            this.all_pieces = document.querySelectorAll(`p.${this.color}`); //プレイヤのボードにある全てのピース
+        };
+    };
+    const black = new Player("black"); //黒側
+    const white = new Player("white"); //白側
 
-    let current_player = black;
-    let opposite_player = white;
+    let current_player = black; //自分のプレイヤ
+    let opposite_player = white; //相手のプレイヤ
 
+    // プレイヤ交換の関数
     const change_turns = () => {
         console.log("change_turns()");
         if (current_player == black) {
-            current_player = white
-            opposite_player = black
+            current_player = white;
+            opposite_player = black;
         } else {
-            current_player = black
-            opposite_player = white
+            current_player = black;
+            opposite_player = white;
         }
         console.log(current_player);
     }
 
-
+    //ボードの場所
     class Point {
         constructor(row, col) {
-            this.row = row
-            this.col = col
+            this.row = row;　 // 縦
+            this.col = col;　 // 横
         }
     }
-    let point = new Point(undefined, undefined)
-    // possible moves checking function
+    let point = new Point(undefined, undefined)　 // Pointのオブジェクト　まだ場所不明でundefined
+
+    // current_player(自分)の横、縦、斜めに置けれる場所を探す関数
     const check = (row, col, direction) => {
         // console.log("check() for possible mves");
-        row_change = direction[0]
-        col_change = direction[1]
+        row_change = direction[0];
+        col_change = direction[1];
         // console.log(`checking (${row}, ${col} for ${row_change}, ${col_change})`);
-        has_opposite = false
+        has_opposite = false;　 // 相手があるかどうか
         let i, j;
         for (i = row + row_change, j = col + col_change; i < 8 && j < 8 && i >= 0 && i >= 0; i += row_change, j += col_change) {
             let piece = table.rows[i].cells[j];
-            if (piece.classList.contains(opposite_player.color)) {
+            if (piece.classList.contains(opposite_player.color)) {　 //もしpieceは相手の石だったら
                 // console.log(`opposite ${opposite_player.color} player found at ${row}, ${i}`);
-                has_opposite = true
-                continue
-            } else if (piece.classList.contains("empty")) {
+                has_opposite = true;　
+                continue;
+            } else if (piece.classList.contains("empty")) {　 //何もなかった場合
                 // console.log(`empty player found at (${piece.parentNode.rowIndex},${piece.cellIndex})`);
-                if (has_opposite) {
-                    piece.appendChild(document.createElement("p"))
-                    piece.children[0].className = "ok"
-                    piece.className = "ok"
+                if (has_opposite) {　 // 前、相手があった場合、
+                    piece.appendChild(document.createElement("p"));　 // 空いていることを表示するピース(p.ok)を入れる
+                    piece.children[0].className = "ok";
+                    piece.className = "ok";
                     console.log(`(${piece.parentNode.rowIndex},${piece.cellIndex}) is OK`);
-                    return
+                    return;
                 }
-                return
+                return;
             } else {
-                return
+                return;
             }
         }
     }
 
+    // 上のcheck()を自分の全部のピースで、横、縦、斜めで確認する関数
     const possible_moves = () => {
         console.log("possible_moves()");
         current_player.all_pieces.forEach(element => {
-            element.parentNode.className = current_player.color
-            row = element.parentNode.parentNode.rowIndex
-            cell = element.parentNode.cellIndex
+            element.parentNode.className = current_player.color;
+            row = element.parentNode.parentNode.rowIndex;
+            cell = element.parentNode.cellIndex;
             directions.forEach(direction => {
-                check(row, cell, direction)
+                check(row, cell, direction);
             });
         });
-    }
+    };
 
+    // check()が入れたp.okを消していく・リセットする関数
     const possible_moves_reset = () => {
         console.log("possible_moves_reset()");
-        const ok_pieces = document.querySelectorAll("td.ok")
+        const ok_pieces = document.querySelectorAll("td.ok");
         ok_pieces.forEach(element => {
-            element.className = "empty"
-            element.removeChild(element.children[0])
+            element.className = "empty";
+            element.removeChild(element.children[0]);
         });
-    }
+    };
 
-
+    // 相手のピースをひっくり返す関数
     const outflank = (row, col, direction) => {
         console.log("outflank()");
         has_opposite = false;
@@ -131,48 +140,40 @@ function DomLoaded(e) {
             return;
         }
     }
-    
-    // function go_ahead() {
-    //     console.log("go_ahead");
-    //     change_turns()
-    //     possible_moves()
-    //     clicked()
-    //     possible_moves_reset()    
-    // } 
 
-    
 
-    let go_ahead = false
-    //watch for a click on an ok spot
+    let go_ahead = false;　 // 関数の全部一気に実行させないために使おうとしています
+
+    //check()でOKされたピースにclickがあったときの関数
     const clicked = () => {
         document.querySelectorAll("td.ok").forEach(element => {
             element.addEventListener('click', (e) => {
-                go_ahead = true
-                console.log("clicked()")
-                point.row = element.parentNode.rowIndex
-                point.col = element.cellIndex
+                go_ahead = true;
+                console.log("clicked()");
+                point.row = element.parentNode.rowIndex;
+                point.col = element.cellIndex;
                 // change element color to current playere color
-                element.children[0].className = current_player.color
-                element.className = current_player.color
+                element.children[0].className = current_player.color;
+                element.className = current_player.color;
                 //outflank
                 directions.forEach(direction => {
-                    outflank(point.row, point.col, direction)
+                    outflank(point.row, point.col, direction);
                 });
-                element.removeEventListener("click", e)
-                possible_moves_reset()
+                element.removeEventListener("click", e);
+                possible_moves_reset();
             })
         });
     }
-    
-    //start the game when the window is ready
-    window.addEventListener("load", ()=>{
-        possible_moves()
-        clicked()
+
+    //windowのloadが終了した後に実行する関数
+    window.addEventListener("load", () => {
+        possible_moves();
+        clicked();
         if (go_ahead) {
-            change_turns()
-            go_ahead = false
-            possible_moves()
-            clicked()
-        }
-    })
+            change_turns();
+            go_ahead = false;
+            possible_moves();
+            clicked();
+        };
+    });
 }

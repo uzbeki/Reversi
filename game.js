856388cc2afe19b643e29b20c10c4b3997dacb42
Beki <uzbekitube@gmail.com>
2020-11-30 18:01:21 +0900

    removeEvLis fixed

--- game.js
+++ game.js
@@ -4,7 +4,6 @@
 const table = document.getElementById("game-table");　 // テーブル
 const table_length = document.getElementById("game-table").rows[0].cells.length;　 //テーブルのサイズ
 const td = document.querySelectorAll("td"); // 全てのtdタッグ
-let game_over = false;
 
 // 横、縦、斜めの確認に必要のある方向の計算し方
 const directions = [
@@ -36,7 +35,6 @@ let opposite_player = white; //相手のプレイヤ
 
 // プレイヤ交換の関数
 const change_turns = () => {
-    console.log("change_turns()");
     if (current_player == black) {
         current_player = white;
         opposite_player = black;
@@ -44,7 +42,15 @@ const change_turns = () => {
         current_player = black;
         opposite_player = white;
     }
-    console.log(current_player);
+
+    console.log("change_turns()");
+    console.log(`current_player is ${current_player.color}`);
+    // document.querySelectorAll("td.ok").forEach(ok => {
+    //     ok.addEventListener('click', piece_clicked, false)
+    // });;
+    // current_player.all_pieces().forEach(piece => {
+    //     piece.children[0].className += "current"
+    // });
 }
 
 //ボードの場所
@@ -76,7 +82,8 @@ const check = (row, col, direction) => {
                 piece.appendChild(document.createElement("p"));　 // 空いていることを表示するピース(p.ok)を入れる
                 piece.children[0].className = "ok";
                 piece.className = "ok";
-                console.log(`(${piece.parentNode.rowIndex},${piece.cellIndex}) is OK`);
+                piece.addEventListener("click", piece_clicked, { once: true });
+                // console.log(`(${piece.parentNode.rowIndex},${piece.cellIndex}) is OK`);
                 return;
             }
             return;
@@ -106,6 +113,7 @@ const possible_moves_reset = () => {
     ok_pieces.forEach(element => {
         element.className = "empty";
         element.removeChild(element.children[0]);
+        element.removeEventListener("click", piece_clicked);
     });
 };
 
@@ -119,19 +127,19 @@ const outflank = (row, col, direction) => {
     for (i = row + row_change, j = col + col_change; i >= 0 && j >= 0 && i < 8 && j < 8; i += row_change, j += col_change) {
         let piece = table.rows[i].cells[j];
         if (piece.className == opposite_player.color) {
-            console.log("has opposite : ");
-            console.log(piece);
+            // console.log("has opposite : ");
+            // console.log(piece);
             has_opposite = true;
             continue;
         } else if (piece.className == current_player.color) {
             if (has_opposite) {
-                console.log(`piece opposite changing`);
-                console.log(point.row, point.col);
-                for (q = point.row + row_change, w = point.col + col_change; q != i || w != j; q += row_change, w += col_change) {
+                // console.log(`piece opposite changing`, point.row, point.col);
+                // for (q = point.row + row_change, w = point.col + col_change; q != i || w != j; q += row_change, w += col_change) {
+                for (q = point.row, w = point.col; q != i || w != j; q += row_change, w += col_change) {
                     table.rows[q].cells[w].className = current_player.color;
                     table.rows[q].cells[w].children[0].className = current_player.color;
                     console.log("piece changed");
-                    console.log(table.rows[q].cells[w]);
+                    // console.log(table.rows[q].cells[w]);
                 }
             }
             return;
@@ -143,29 +151,34 @@ const outflank = (row, col, direction) => {
 
 let go_ahead = false;　 // 関数の全部一気に実行させないために使おうとしています
 
-//check()でOKされたピースにclickがあったときの関数
-const clicked = () => {
-    document.querySelectorAll("td.ok").forEach(element => {
-        element.addEventListener('click', (e) => {
-            go_ahead = true;
-            console.log("clicked()");
-            point.row = element.parentNode.rowIndex;
-            point.col = element.cellIndex;
-            // change element color to current playere color
-            element.children[0].className = current_player.color;
-            element.className = current_player.color;
-            //outflank
-            directions.forEach(direction => {
-                outflank(point.row, point.col, direction);
-            });
-            element.removeEventListener("click", e);
-            possible_moves_reset();
-        })
+function piece_clicked(e) {
+    console.log(e);
+    if (e.path[0].childElementCount == 0) {
+        point.row = e.path[2].rowIndex
+        point.col = e.path[1].cellIndex
+        console.log(`${point.row}, ${point.col} clicked`);
+    } else {
+        point.row = e.path[1].rowIndex;
+        point.col = e.path[0].cellIndex;
+        console.log(`${point.row}, ${point.col} clicked`);
+    }
+
+
+    // piece.children[0].className = current_player.color;
+    // piece.className = current_player.color;
+    //outflank
+    directions.forEach(direction => {
+        outflank(point.row, point.col, direction);
     });
+    possible_moves_reset();
+    change_turns();
+    possible_moves();
 }
 
 possible_moves();
-clicked();
+
+// change_turns()
+// possible_moves()
 /* やりたい流れ:
 window.onload{
     // possible_moves();
